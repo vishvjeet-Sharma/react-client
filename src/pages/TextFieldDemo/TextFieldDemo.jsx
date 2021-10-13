d6d498d6122618f3208505b2a9ad9ae67bf4fc22
Vishvjeet-Sharma <vishvjeet.sharma@successive.tech>
2021-10-13 22:41:10 +0530

    Task completed

--- src/pages/TextFieldDemo/TextFieldDemo.jsx
+++ src/pages/TextFieldDemo/TextFieldDemo.jsx
@@ -5,8 +5,8 @@ const TextFieldDemo = () => (
   <div>
     <TextField heading="This is a Disabld Input" value="Disabled Input" disabled />
     <TextField heading="A Valid Input" value="Accessible" />
-    <TextField heading="An Input with errors" value="101" error="Could not be greater than" />
+    <TextField heading="An Input with error" value="101" error="Could not be greater than" />
   </div>
 );
 
-export default TextFieldDemo;
+export default TextFieldDemo; 
