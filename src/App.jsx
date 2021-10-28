2ba054d11d6b0d05cf7b8adf464c5ea6ad3037f3
Vishvjeet-Sharma <vishvjeet.sharma@successive.tech>
2021-10-28 16:31:40 +0530

    Day 9 Task Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 //import { InputDemo } from './pages/index';
 import { ThemeProvider } from '@mui/material/styles';
-import { ChildrenDemo } from './pages/index'
+// import { ChildrenDemo } from './pages/index'
+import { Trainee } from './pages/index'
 import { theme } from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
     <div className="App">
-      <ChildrenDemo />
+      <Trainee />
     </div>
     </ThemeProvider>
   );
