a871e40c761a1c5b030693d38d194bb66ad1c1b5
Vishvjeet-Sharma <vishvjeet.sharma@successive.tech>
2021-11-10 13:59:18 +0530

    Day 11: React - Basic Route Setup and Layouts

--- src/components/SelectField/SelectField.jsx
+++ src/components/SelectField/SelectField.jsx
@@ -1,37 +1,36 @@
 import React from 'react';
-import { PropTypes } from 'prop-types';
+import PropTypes from 'prop-types';
 import './style.css';
 
-
-const Selectfield = (props) => {
-    const {
-        error, value, options, defaultText, onChange,
-    } = props;
-    return (
-        <label htmlFor="sport">
-            <h3>Select the game you play.</h3>
-            <select className="sport" id="sport" name="sport" value={value} onChange={onChange} error={error}>
-                <option className="sport" value="">{defaultText}</option>
-                {options.map((sport) => {
-                    const { value: selectvalue, label } = sport;
-                    return <option className="sport" key={selectvalue} value={selectvalue}>{label}</option>;
-                })}
-            </select>
-        </label>
-    );
+const SelectField = (props) => {
+  const {
+    error, value, onChange, options, defaultText,
+  } = props;
+  return (
+    <label htmlFor="sport">
+      <h4>Select the game you play?</h4>
+      <select className="sport" id="sport" name="sport" value={value} onChange={onChange} error={error}>
+        <option className="sport" value="">{defaultText}</option>
+        {options.map((sport) => {
+          const { value: selectvalue, label } = sport;
+          return <option className="sport" key={selectvalue} value={selectvalue}>{label}</option>;
+        })}
+      </select>
+    </label>
+  );
 };
 
-Selectfield.defaultProps = {
-    error: '', defaultText: 'Select', options: [],
+SelectField.defaultProps = {
+  error: '',
+  defaultText: 'Select',
+  options: [],
 };
 
-Selectfield.propTypes = {
-    error: PropTypes.string,
-    value: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-    option: PropTypes.arrayOf(PropTypes.object),
-    defaultText: PropTypes.string,
-
+SelectField.propTypes = {
+  error: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.arrayOf(PropTypes.object),
+  defaultText: PropTypes.string,
 };
-
-export default Selectfield;
+export default SelectField;
