1588dac918c84e402214d1fae5133e3ce685b80a
Vishvjeet-Sharma <vishvjeet.sharma@successive.tech>
2021-11-30 16:21:57 +0530

    Day 12 - React - Dynamic Routing

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,3 @@ export { SelectField } from './SelectField/index';
 export { RadioGroup } from './RadioGroup/index';
 export { Math } from './Math/index';
 export { TextField1 } from './TextField1/index';
-export { GenericTable } from './Table/index';
